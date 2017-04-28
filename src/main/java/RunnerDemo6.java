a32ec2da6c6bb42dfbde51aca1930224386ff0a5
Mykola_Fedorych <????????02>
2017-04-28 16:32:10 +0300

    erl coder and decoder added task finished RPN in progress

--- src/main/java/RunnerDemo6.java
+++ src/main/java/RunnerDemo6.java
@@ -2,16 +2,11 @@ import demo6.ERL;
 
 public class RunnerDemo6 {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         //Initializing
 //        CalculationOfRPN calculationOfRPN = new CalculationOfRPN();
         ERL erl = new ERL();
         System.out.println(erl.coder("Heeeeeeeeeeeeeeellooooo"));
         System.out.println(erl.decoder("H9e3e2l5o"));
-//        System.out.println(erl.coderRLE("weeeeeeeeeeeeerkooooo"));
-//        System.out.println(erl.decoder("4e5y"));
-//        //Compressing and Decoding
-//        System.out.println(erl.compressor(""));
-//        System.out.println(erl.decoding(""));
     }
 }
