d2bcdef29efd64241d2bfad1043d61db08141124
Fed <fedkoggot@gmail.com>
2017-06-22 14:53:32 +0300

    xpath changed to css

--- src/main/java/sc/DriverUtils.java
+++ src/main/java/sc/DriverUtils.java
@@ -1,7 +1,9 @@
 package sc;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class DriverUtils {
 
@@ -13,6 +15,26 @@ public class DriverUtils {
             String exePath = "drivers/geckodriver.exe";
             System.setProperty("webdriver.gecko.driver", exePath);
             driver = new FirefoxDriver();
+        }
+        return driver;
+    }
+
+    public static WebDriver getOperaDriver() {
+
+        if (driver != new OperaDriver()) {
+            String exePath = "drivers/operadriver_win64/operadriver.exe";
+            System.setProperty("webdriver.opera.driver", exePath);
+            driver = new OperaDriver();
+        }
+        return driver;
+    }
+
+    public static WebDriver getChromeDriver() {
+
+        if (driver != new OperaDriver()) {
+            String exePath = "drivers/chromedriver.exe";
+            System.setProperty("webdriver.opera.driver", exePath);
+            driver = new ChromeDriver();
         }
         return driver;
     }
