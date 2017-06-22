45057fd8ee1b7400c43f195e306b8cf587a6c9bb
Sergii Stepanov <Sergii_Stepanov@epam.com>
2017-06-22 14:35:13 +0300

    Update project structure with WebDriver

--- src/main/java/sc/pages/LogInPage.java
+++ src/main/java/sc/pages/LogInPage.java
@@ -6,27 +6,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import static sc.DriverUtils.getFoxDriver;
 
-public class LogInPage {
+public class LogInPage extends BasePage {
 
-    private WebDriver driver = getFoxDriver();
+    @FindBy(id = "steamAccountName")
+    private WebElement steamAccountNameButtonBy;
+    @FindBy(id = "steamPassword")
+    private WebElement steamAccountPasswordButtonBy;
+    @FindBy(id = "imageLogin")
+    private WebElement steamSignInButtonBy;
+    @FindBy(css = "button[class=\"btn btn-signin\"]")
+    private WebElement scSignInButtonBy;
 
-    public LogInPage() {
-        PageFactory.initElements(driver, this);
+    public LogInPage(WebDriver driver) {
+        super(driver);
     }
 
-   @FindBy(id = "steamAccountName")
-    private WebElement steamAccountNameButtonBy;
-   @FindBy(id = "steamPassword")
-    private WebElement steamAccountPasswordButtonBy;
-   @FindBy(id = "imageLogin")
-    private WebElement steamSignInButtonBy;
-   @FindBy(css = "button[class=\"btn btn-signin\"]")
-   private WebElement scSignInButtonBy;
-
-    public WebElement getScSignInButton() {
-             return scSignInButtonBy;
+    public WebElement getLogInButton() {
+        return scSignInButtonBy;
     }
     public WebElement getSteamSignInButton() {
         return steamSignInButtonBy;
@@ -38,23 +35,22 @@ public class LogInPage {
         return steamAccountNameButtonBy;
     }
 
-
-//    public By steamAccountNameButtonBy = By.xpath("//*[@id='steamAccountName']");
-//    public By steamAccountPasswordButtonBy = By.xpath("//*[@id='steamPassword']");
-//    public By steamSignInButtonBy = By.xpath("//*[@id='imageLogin']");
-//    public By scSignInButtonBy = By.xpath("//*[@class='btn btn-signin']");
-//
-//    public WebElement getScSignInButton() {
-//        return driver.findElement(scSignInButtonBy);
-//    }
-//    public WebElement getSteamSignInButton() {
-//        return driver.findElement(steamSignInButtonBy);
-//    }
-//    public WebElement getSteamAccountPasswordButton() {
-//        return driver.findElement(steamAccountPasswordButtonBy);
-//    }
-//    public WebElement getSteamAccountNameButton() {
-//        return driver.findElement(steamAccountNameButtonBy);
-//    }
+    // public By steamAccountNameButtonBy = By.xpath("//*[@id='steamAccountName']");
+    // public By steamAccountPasswordButtonBy = By.xpath("//*[@id='steamPassword']");
+    // public By steamSignInButtonBy = By.xpath("//*[@id='imageLogin']");
+    // public By scSignInButtonBy = By.xpath("//*[@class='btn btn-signin']");
+    //
+    // public WebElement getLogInButton() {
+    // return driver.findElement(scSignInButtonBy);
+    // }
+    // public WebElement getSteamSignInButton() {
+    // return driver.findElement(steamSignInButtonBy);
+    // }
+    // public WebElement getSteamAccountPasswordButton() {
+    // return driver.findElement(steamAccountPasswordButtonBy);
+    // }
+    // public WebElement getSteamAccountNameButton() {
+    // return driver.findElement(steamAccountNameButtonBy);
+    // }
 }
 
--- src/main/java/webDriver/LogInTest.java
+++ src/main/java/webDriver/LogInTest.java
@@ -1,19 +1,25 @@
 package webDriver;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
-import sc.DriverUtils;
+
 import sc.actions.LogInPageActions;
 import sc.pages.LogInPage;
 
+import static sc.DriverUtils.getFoxDriver;
+
 public class LogInTest {
 
-    private WebDriver driver = DriverUtils.getFoxDriver();
-    private LogInPageActions logInPageActions = new LogInPageActions();
-    private LogInPage logInPage = new LogInPage();
+    LogInPageActions logInPageActions = new LogInPageActions();
+    private WebDriver driver = getFoxDriver();
+    LogInPage logInPage = new LogInPage(driver);
 
     public void LoggingSteps() throws Throwable {
 
         driver.get("https://skins.cash");
+        driver.manage().timeouts().pageLoadTimeout(5000, TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
 
         logInPageActions.clickScSignInButton();
 
@@ -23,6 +29,7 @@ public class LogInTest {
             driver.switchTo().window(winHandle); // switch focus of WebDriver to the next found window handle (that's your newly opened window)
         }
 
+        // Put this to the Action class
         logInPage.getSteamAccountNameButton().sendKeys("shumisteam001");
         logInPage.getSteamAccountPasswordButton().sendKeys("8GLtSBfhYzICQENO");
         logInPage.getSteamSignInButton().click();
